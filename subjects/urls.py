9e8e1bd756aaa386e864d0a372badf6c77651d68
8JotaScript8 <jotagebe07@gmail.com>
2026-03-15 22:36:44 -0300

    final adjustments 15/03/2026

--- subjects/urls.py
+++ subjects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SubjectCreateView, SubjectListView
+from .views import SubjectCreateView, SubjectDeleteView, SubjectListView
 
 app_name = 'subjects'
 
 urlpatterns = [
     path('', SubjectListView.as_view(), name='subject_list'),   
     path('create/', SubjectCreateView.as_view(), name='subject_create'),
+    path('delete/<int:pk>/', SubjectDeleteView.as_view(), name='subject_delete'),
 ]
--- subjects/views.py
+++ subjects/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView
 
 import subjects
 from .models import Subject
@@ -9,17 +9,35 @@ class SubjectCreateView(LoginRequiredMixin, CreateView):
     model = Subject
     fields = ['name']
     template_name = 'subjects/subject_form.html'
-    success_url = reverse_lazy('task_list')
+    success_url = reverse_lazy('subjects:subject_list')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+
+        if next_url:
+            return next_url
+        
+
+        return super().get_success_url()
     
 
 class SubjectListView(LoginRequiredMixin, ListView):
     model = Subject
     template_name = 'subjects/subject_list.html'
     context_object_name = 'subjects'
+
+    def get_queryset(self):
+        return Subject.objects.filter(user=self.request.user)
+
+
+class SubjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Subject
+    template_name = 'subjects/subject_confirm_delete.html'
+    success_url = reverse_lazy('subjects:subject_list')
 
     def get_queryset(self):
         return Subject.objects.filter(user=self.request.user)
